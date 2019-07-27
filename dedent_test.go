a9273cc309e373abc321350e011e576f49c8c66b
Bo Maryniuk <bo@suse.de>
2019-07-27 14:10:57 +0200

    Add more dedent unit tests

--- dedent_test.go
+++ dedent_test.go
@@ -22,3 +22,41 @@ func TestDedentMultilineNoEmpty(t *testing.T) {
 	expected := "two spaces\n  four spaces\n    six spaces\n three spaces"
 	assert.Equal(t, NewTextWrap().Dedent(multiline), expected)
 }
+
+/*
+  Test text dedent with multiple lines,
+  containing tabs.
+*/
+func TestDedentMultilineMixedSpace(t *testing.T) {
+	multiline := "\ttab\n onespace"
+	expected := "   tab\nonespace"
+	assert.Equal(t, NewTextWrap().Dedent(multiline), expected)
+}
+
+/*
+  Test text dedent with multiple lines,
+  containing tabs, different tab space
+*/
+func TestDedentMultilineTabSpace(t *testing.T) {
+	multiline := "\ttab\n onespace"
+	expected := "       tab\nonespace"
+	assert.Equal(t, NewTextWrap().SetTabSpacesWidth(8).Dedent(multiline), expected)
+}
+
+/*
+  Test text dedent without dropping tabs, where whitespace contains only tabs.
+*/
+func TestDedentMultilineNoTabsDropNoSpace(t *testing.T) {
+	multiline := "\ttab\n onespace"
+	expected := "\ttab\n onespace"
+	assert.Equal(t, NewTextWrap().SetExpandTabs(false).Dedent(multiline), expected)
+}
+
+/*
+  Test text dedent without dropping tabs, where whitespace is mixed.
+*/
+func TestDedentMultilineNoTabsDrop(t *testing.T) {
+	multiline := " \ttab\n  onespace"
+	expected := "\ttab\n onespace"
+	assert.Equal(t, NewTextWrap().SetExpandTabs(false).Dedent(multiline), expected)
+}
